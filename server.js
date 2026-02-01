7c3a67a1284acb359209c36674fd5fb9b804e5d3
Yerulan <aidala.damyn@gmail.com>
2026-02-01 15:53:49 +0500

    refactor: transition to modular MVC architecture

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const Project = require('./models/Project'); 
+const projectRoutes = require('./routes/projectRoutes');
+const categoryRoutes = require('./routes/categoryRoutes'); 
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -16,63 +18,9 @@ mongoose.connect(process.env.MONGODB_URI)
     .catch(err => console.error('Connection error', err));
 
 
-
-app.post('/projects', async (req, res) => {
-    try {
-        const newProject = new Project(req.body);
-        const savedProject = await newProject.save();
-        res.status(201).json(savedProject);
-    } catch (err) {
-        res.status(400).json({ message: "Ошибка валидации: " + err.message });
-    }
-});
-
-
-app.get('/projects', async (req, res) => {
-    try {
-        const projects = await Project.find();
-        res.json(projects);
-    } catch (err) {
-        res.status(500).json({ message: "Ошибка сервера" });
-    }
-});
-
-
-app.get('/projects/:id', async (req, res) => {
-    try {
-        const project = await Project.findById(req.params.id);
-        if (!project) return res.status(404).json({ message: "Проект не найден" });
-        res.json(project);
-    } catch (err) {
-        res.status(400).json({ message: "Некорректный ID" });
-    }
-});
-
-
-app.put('/projects/:id', async (req, res) => {
-    try {
-        const updated = await Project.findByIdAndUpdate(
-            req.params.id, 
-            req.body, 
-            { new: true, runValidators: true }
-        );
-        if (!updated) return res.status(404).json({ message: "Проект не найден" });
-        res.json(updated);
-    } catch (err) {
-        res.status(400).json({ message: "Ошибка обновления: " + err.message });
-    }
-});
-
-
-app.delete('/projects/:id', async (req, res) => {
-    try {
-        const deleted = await Project.findByIdAndDelete(req.params.id);
-        if (!deleted) return res.status(404).json({ message: "Проект не найден" });
-        res.json({ message: "Проект успешно удален" });
-    } catch (err) {
-        res.status(500).json({ message: "Ошибка при удалении" });
-    }
-});
+app.use('/auth', authRoutes); 
+app.use('/projects', projectRoutes);
+app.use('/categories', categoryRoutes); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
