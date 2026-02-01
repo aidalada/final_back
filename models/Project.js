cdf038a1bc7105f136db3ce951ec4b248327372b
Yerulan <aidala.damyn@gmail.com>
2026-02-01 15:53:14 +0500

    feat: implement Mongoose schemas for User, Project, and Category

--- models/Project.js
+++ models/Project.js
@@ -12,9 +12,14 @@ const projectSchema = new mongoose.Schema({
     budget: { 
         type: Number, 
         required: [true, 'Бюджет обязателен'] 
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category', 
+        required: false 
     }
 }, { 
-    timestamps: true // Это автоматически создаст createdAt и updatedAt
+    timestamps: true 
 });
 
 module.exports = mongoose.model('Project', projectSchema);
