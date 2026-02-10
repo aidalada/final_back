ab5d7bc301b8385a11a1a73c70125602bd34f53a
Yerulan <aidala.damyn@gmail.com>
2026-02-11 02:09:26 +0500

    feat: secure category routes with role-based access control

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,8 +4,6 @@ const categoryController = require('../controllers/categoryController');
 
 const { authenticate, authorizeAdmin } = require('../middleware/authMiddleware');
 router.get('/', categoryController.getAllCategories);
-router.post('/', categoryController.createCategory);
-router.delete('/:id', categoryController.deleteCategory);
 router.post('/', authenticate, authorizeAdmin, categoryController.createCategory);
 router.delete('/:id', authenticate, authorizeAdmin, categoryController.deleteCategory);
 
